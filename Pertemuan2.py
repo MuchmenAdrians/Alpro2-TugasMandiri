fd1c888d1319ee10488e0f725bfc56c6a456db47
MUCH. MENTARI ADRIANSYAH <muchmenadriansyah@gmail.com>
2026-05-22 18:36:50 +0700

    Update Pertemuan2.py

--- Pertemuan2.py
+++ Pertemuan2.py
@@ -1,5 +1,78 @@
-code = {}
-if len(code) == 0:
-    print(True)
-else:
-    print(False)
+# MATERI PERTEMUAN 2: Python Literals and Operators
+
+# 1
+print("HELLO, THIS IS MY FIRST PYTHON PROGRAM")
+print(2026)
+
+# 2
+print(11111111)
+print(-11111111)
+
+# 3
+print(0x2A) # Hexadecimal representation of 32 in decimal
+print(0o20) # Octal representation of 16 in decimal
+
+# 4
+print(0.4)
+print(4.0)
+print(.4)
+print(4.)
+print(4e-1)
+print(4e0)
+
+# 5
+print("This is my first Python program")
+print('Second line of code')
+
+# 6
+print(True)
+print(False)
+
+# 7
+print(2**3) # Integer-Integer
+print(2.0**3) # Float-Integer
+print(2**3.0) # Integer-Float
+print(2.0**3.0) # Float-Float
+
+# 8
+print(2*10) #integer-integer
+print(2.5*10) #float-integer
+print(2*10.5) #integer-float
+print(2.5*10.5) #float-float
+
+# 9
+print(10/2) #Integer-Integer
+print(10/2.0) #Integer-Float
+print(10.0/2) #Float-Integer
+print(10.0/2.0) #Float-Float
+
+# 10
+print(20//2) #Integer-integer
+print(20//2.0) #Integer-float
+print(20.0//2) #Float-integer
+print(20.0//2.0) #Float-float
+
+# 11
+print(18%3) #Integer-Integer
+print(18%3.0) #Integer-Float
+print(18.0%5) #Float-Integer
+print(18.0%5.0) #Float-Float
+
+# 12
+print(+10) #unary plus
+print(-10) #unary minus
+print(10 + 5) #Binary addition
+print(10 - 5) #Binary subtraction
+print(10 * 5) #Binary multiplication
+print(10 / 5) #Binary division
+print(10 % 3) #Modulus
+print(10 ** 2) #Exponentiation
+print(10 // 3) #Floor division
+
+# 13
+print((10 + 5) * (10 - 5) + 10 / 5)
+
+# 14
+print((2**4), (2*4.), (2*4))
+print((-2/4), (2/4), (2//4), (-2//4))
+print((2%-4), (2%4), (2**3**2))
